cf78dd37df20c47b0b4a17cfd44c600533a10220
Jan Macenka <jan@macenka.de>
2023-10-05 22:09:30 +0200

    Adapted logo

--- src/quality-tester/config.py
+++ src/quality-tester/config.py
@@ -5,7 +5,7 @@ default_config = {
     'min_wait_time': 10000, # Minimum duration in milliseconds for Test-simulation
     'max_wait_time': 5000,  # Maximum duration in milliseconds for Test-simulation
     'default_user': 'pi', # Default user, needs to be pi, if you want to use a different user some adaptions to the implementation are required
-    'logo_name': 'logo.png', # Name of the logo to be used
+    'logo_name': 'kostal-logo.png', # Name of the logo to be used
     'password': 'updateplease', # Default password can be manually change in src/quality-tester/static/configuration/config.yaml
     'simulate_pictures':False, # Disable the real scan for picture and always just return a simulated result
 }
